e6cae8411c8091398d99acc0d2071f4210d72965
nagiyu <otonagiyu@gmail.com>
2024-12-15 01:12:30 +0900

    Remove S3 bucket creation from initialization script and update LocalStack image version in Docker Compose

--- containers/aws/init.sh
+++ containers/aws/init.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# 必要なS3バケットを作成
-awslocal s3api create-bucket --bucket health
-
 # 必要なDynamoDBテーブルを作成する
 awslocal dynamodb create-table \
   --table-name my-table \
   --attribute-definitions AttributeName=id,AttributeType=S \
   --key-schema AttributeName=id,KeyType=HASH \
-  --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+  --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
+  --region ap-northeast-3
