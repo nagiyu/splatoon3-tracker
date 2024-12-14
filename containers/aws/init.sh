d138e4d64c6dfac508204a702c3e0dd7c23e3b58
nagiyu <otonagiyu@gmail.com>
2024-12-15 01:26:06 +0900

    Remove region specification from DynamoDB table creation in initialization script

--- containers/aws/init.sh
+++ containers/aws/init.sh
@@ -5,5 +5,4 @@ awslocal dynamodb create-table \
   --table-name my-table \
   --attribute-definitions AttributeName=id,AttributeType=S \
   --key-schema AttributeName=id,KeyType=HASH \
-  --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
-  --region ap-northeast-3
+  --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
