c71215c62cddb34a4d6af2e6481c3aa543c4656e
nagiyu <otonagiyu@gmail.com>
2024-12-15 02:42:32 +0900

    Add SSL certificates and update Nginx configuration for HTTPS support

--- containers/aws/init.sh
+++ containers/aws/init.sh
@@ -5,6 +5,20 @@ awslocal dynamodb create-table \
   --table-name Auth \
   --attribute-definitions AttributeName=UserId,AttributeType=S \
   --key-schema AttributeName=UserId,KeyType=HASH \
+  --global-secondary-indexes \
+    '[
+      {
+        "IndexName": "GoogleUserId-index",
+        "KeySchema": [
+          { "AttributeName": "GoogleUserId", "KeyType": "HASH" }
+        ],
+        "Projection": {
+          "ProjectionType": "INCLUDE",
+          "NonKeyAttributes": ["UserId", "SampleRole"]
+        },
+        "ProvisionedThroughput": null
+      }
+    ]' \
   --billing-mode PAY_PER_REQUEST \
   --table-class STANDARD \
   --sse-specification Enabled=true,SSEType=AES256
