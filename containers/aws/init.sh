1447fbe3503cfba852bd852ae1a201976df59818
nagiyu <otonagiyu@gmail.com>
2024-12-15 00:08:45 +0900

    Update deployment configuration and add sample Nginx settings for LocalStack integration

---
+++ containers/aws/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# 必要なDynamoDBテーブルを作成する
+awslocal dynamodb create-table \
+  --table-name my-table \
+  --attribute-definitions AttributeName=id,AttributeType=S \
+  --key-schema AttributeName=id,KeyType=HASH \
+  --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
