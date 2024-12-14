1e3ce70cb02a13706395e31c4042bda79167500c
nagiyu <otonagiyu@gmail.com>
2024-12-15 01:48:35 +0900

    Update initialization script to create 'Auth' DynamoDB table with enhanced specifications

--- containers/aws/init.sh
+++ containers/aws/init.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# 必要なDynamoDBテーブルを作成する
+# DynamoDB テーブル「Auth」を作成
 awslocal dynamodb create-table \
-  --table-name my-table \
-  --attribute-definitions AttributeName=id,AttributeType=S \
-  --key-schema AttributeName=id,KeyType=HASH \
-  --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+  --table-name Auth \
+  --attribute-definitions AttributeName=UserId,AttributeType=S \
+  --key-schema AttributeName=UserId,KeyType=HASH \
+  --billing-mode PAY_PER_REQUEST \
+  --table-class STANDARD \
+  --sse-specification Enabled=true,SSEType=AES256
