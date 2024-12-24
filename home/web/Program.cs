846dbc935bbf0ba8d678bb2827e9022d89237927
nagiyu <otonagiyu@gmail.com>
2024-12-25 00:35:50 +0900

    Revert "Skip HTTPS redirection for health check endpoint"
    This reverts commit ccbcb727a3d0a04c205c83af84161636110fa072.

--- home/web/Program.cs
+++ home/web/Program.cs
@@ -68,22 +68,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-if (!app.Environment.IsDevelopment())
-{
-    app.Use(async (context, next) =>
-    {
-        // ヘルスチェックのパスの場合、HTTPSリダイレクトをスキップ
-        if (context.Request.Path.StartsWithSegments("/health-check"))
-        {
-            await next();
-        }
-        else
-        {
-            context.Response.Redirect($"https://{context.Request.Host}{context.Request.Path}");
-        }
-    });
-}
-
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
